04d8d3e94732434f87dca49bae4adce51e1a3be0
namratha-p-s <namratha.psom.97@gmail.com>
2023-10-18 20:18:41 -0600

    Initial Commit

---
+++ Feed/FeedApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeedApp.swift
+//  Feed
+//
+//  Created by Namratha P Somachudan on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct FeedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
